c82be1f2fbde2d479ec9c94c42a3f2cb59f8b72b
11 <270680516@qq.com>
2021-10-27 17:45:36 +0800

    feat: hello pnpm

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,16 +7,91 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 12,
+    ecmaVersion: 2021,
     sourceType: 'module'
   },
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off'
-  }
+    'react/react-in-jsx-scope': 'off',
+    eqeqeq: ['warn', 'always', { null: 'never' }],
+    'no-debugger': ['error'],
+    'no-empty': ['warn', { allowEmptyCatch: true }],
+    'no-process-exit': 'off',
+    'no-useless-escape': 'off',
+    'prefer-const': [
+      'warn',
+      {
+        destructuring: 'all'
+      }
+    ],
+
+    'node/no-missing-import': [
+      'error',
+      {
+        allowModules: ['types', 'estree', 'testUtils', 'less', 'sass', 'stylus'],
+        tryExtensions: ['.ts', '.js', '.jsx', '.tsx', '.d.ts']
+      }
+    ],
+    'node/no-missing-require': [
+      'error',
+      {
+        // for try-catching yarn pnp
+        allowModules: ['pnpapi', 'vite'],
+        tryExtensions: ['.ts', '.js', '.jsx', '.tsx', '.d.ts']
+      }
+    ],
+    'node/no-restricted-require': [
+      'error',
+      Object.keys(require('./packages/vite/package.json').devDependencies).map((d) => ({
+        name: d,
+        message:
+          `devDependencies can only be imported using ESM syntax so ` +
+          `that they are included in the rollup bundle. If you are trying to ` +
+          `lazy load a dependency, use (await import('dependency')).default instead.`
+      }))
+    ],
+    'node/no-extraneous-import': [
+      'error',
+      {
+        allowModules: ['vite', 'less', 'sass']
+      }
+    ],
+    'node/no-extraneous-require': [
+      'error',
+      {
+        allowModules: ['vite']
+      }
+    ],
+    'node/no-deprecated-api': 'off',
+    'node/no-unpublished-import': 'off',
+    'node/no-unpublished-require': 'off',
+    'node/no-unsupported-features/es-syntax': 'off',
+
+    '@typescript-eslint/ban-ts-comment': 'off', // TODO: we should turn this on in a new PR
+    '@typescript-eslint/ban-types': 'off', // TODO: we should turn this on in a new PR
+    '@typescript-eslint/no-empty-function': ['error', { allow: ['arrowFunctions'] }],
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-explicit-any': 'off', // maybe we should turn this on in a new PR
+    '@typescript-eslint/no-extra-semi': 'off', // conflicts with prettier
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off', // maybe we should turn this on in a new PR
+    '@typescript-eslint/no-unused-vars': 'off', // maybe we should turn this on in a new PR
+    '@typescript-eslint/no-var-requires': 'off'
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'off'
+      }
+    },
+    {
+      files: ['*.d.ts'],
+      rules: {
+        '@typescript-eslint/triple-slash-reference': 'off'
+      }
+    }
+  ]
 }
